e2db5954f7eb285ca341eba7665830e59e8acbd4
infantito <danielinfantito61@gmail.com>
2019-05-18 19:51:09 -0500

    [add] production

---
+++ backend/app/public/precache-manifest.f02d6079cbe2af34f1fcfade73950dc4.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "766fe17ebeaed33f6b2ca8dea6c83d36",
+    "url": "/index.html"
+  },
+  {
+    "revision": "acee4dad76b23c1725c8",
+    "url": "/static/js/2.dbf9eb78.chunk.js"
+  },
+  {
+    "revision": "b3bbbdd71d8957aab6d1",
+    "url": "/static/js/main.e72533ce.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
